90de83f74d97ca97089d871b4f8745958a9ac22f
Gabriel <gabrielhong119@gmail.com>
2025-11-11 12:52:44 +0800

    Initialise repo

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///clash_of_code.db'
+app.config['SECRET_KEY'] = 'dev_secret_key'  # change later for production
+
+db = SQLAlchemy(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
